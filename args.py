fbf5e7b36297724493a8eae2b51e5e3199dea3e8
ashkanfard132 <160322126+ashkanfard132@users.noreply.github.com>
2025-07-09 22:08:52 +0100

    Update args.py
    scheduler update

--- args.py
+++ args.py
@@ -30,7 +30,7 @@ def get_args():
     parser.add_argument('--optim', type=str, default='adam',
                         choices=['adam', 'sgd', 'rmsprop', 'adamw', 'amsgrad'])
     parser.add_argument('--sched', type=str, default='step',
-                        choices=['step', 'exp', 'plateau', 'cosine'])
+                        choices=['step', 'exp', 'plateau', 'cosine', 'none'])
     parser.add_argument('--step_size', type=int, default=10)
     parser.add_argument('--t_max', type=int, default=10,
                         help='T_max for cosine annealing scheduler')
